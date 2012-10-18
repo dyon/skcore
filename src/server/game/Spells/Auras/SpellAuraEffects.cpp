7b10acbf7417be6fa636d7d5a4401efaf44f87f7
adrian <box758sk@hotmail.com>
2012-10-18 23:19:05 +0200

    Revert https://github.com/aedansilver/TrinityCore/commit/f93f91a5aa354ff68761edb407997cfacb8edfad#src/server/scripts/World/npcs_special.cpp

--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -5678,15 +5678,6 @@ void AuraEffect::HandlePeriodicDummyAuraTick(Unit* target, Unit* caster) const
                         target->RemoveAura(64821);
                     }
                     break;
-                case 67039: // argent squire mount
-                    if (caster && caster->GetOwner())
-                    {
-                        if (caster->GetOwner()->IsMounted())
-                            caster->Mount(29736);
-                        else if (caster->IsMounted())
-                            caster->Dismount();
-                    }
-                    break;
             }
             break;
         case SPELLFAMILY_MAGE:
