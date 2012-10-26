ed251ff223c5bf86c02c03651e4b586adceffd1f
adrian <box758sk@hotmail.com>
2012-10-26 22:15:09 +0200

    Fix quest 12537 - 12540 - 12570 And item Zepik-dajik Author Q12537: Xanvial. Author Q12540: Valcorb. Author Q12570: untaught. Author Item Zepik and Dajik: gecko32. THX

---
+++ sql/core/quests/Q12537-Lightning_Definitely_Strikes_Twice.sql
@@ -0,0 +1,4 @@
+-- Lightning Definitely Strikes Twice
+DELETE FROM `event_scripts` WHERE `id`=18475 AND `command`=7;
+INSERT INTO `event_scripts` (`id`, `delay`, `command`, `datalong`, `datalong2`, `dataint`, `x`, `y`, `z`, `o`) VALUES 
+(18475, 0, 7, 12537, 0, 0, 0, 0, 0, 0);
