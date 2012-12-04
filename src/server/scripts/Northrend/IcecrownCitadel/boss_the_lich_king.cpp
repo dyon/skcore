901c603475222484a90fa950a81ddda057ba051b
adrian <box758sk@hotmail.com>
2012-12-04 18:59:49 +0100

    Some correction tlk

--- src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_the_lich_king.cpp
@@ -1529,12 +1529,6 @@ class npc_valkyr_shadowguard : public CreatureScript
                 // schedule siphon life event (heroic only)
                 DoZoneInCombat();
                 _events.Reset();
-                me->SetCanFly(true);
-                me->SetDisableGravity(true);
-                Position pos;
-                pos.Relocate(me);
-                pos.m_positionZ += 15.0f;
-                me->GetMotionMaster()->MoveTakeoff(1, pos);
                 _events.ScheduleEvent(EVENT_LIFE_SIPHON, 2000);
             }
 
@@ -1551,7 +1545,8 @@ class npc_valkyr_shadowguard : public CreatureScript
                 {
                     case POINT_DROP_PLAYER:
                         me->GetPosition(&_current);
-                        if (_current.GetExactDist(&_dropPoint) != 0)
+                        me->GetPosition(&_current);
+                        if (_current.GetPositionX() != _dropPoint.GetPositionX() || _current.GetPositionY() != _dropPoint.GetPositionY())
                         {
                             _events.ScheduleEvent(EVENT_MOVE_TO_DROP_POS, 0);
                             break;
