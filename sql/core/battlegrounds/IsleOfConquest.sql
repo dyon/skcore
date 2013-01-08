1d04303f4416bbc7907cedb9ff3c3313fa56cb57
adrian <box758sk@hotmail.com>
2013-01-08 17:57:27 +0100

    Revert "Fix IoC teleports"
    This reverts commit a998c1f8808b022bebedd6da0f54e05e93accd3e.

--- sql/core/battlegrounds/IsleOfConquest.sql
+++
@@ -1,3 +0,0 @@
--- Fix Isle of Conquest - Teleports
-UPDATE `spell_linked_spell` SET `spell_effect` = 66551 WHERE `spell_trigger` IN (66549, 66548);
-UPDATE `spell_linked_spell` SET `spell_trigger` = 66551 WHERE `spell_effect` IN (-66548, -66549);
