de81e40682299657e0a9c5c552c3f1ff67cd15cc
adrian <box758sk@hotmail.com>
2013-03-09 11:11:28 +0100

    Corregido el talento de Succubo mejorada

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3158,6 +3158,24 @@ void SpellMgr::LoadDbcDataCorrections()
                 break;
             // Master Shapeshifter: missing stance data for forms other than bear - bear version has correct data
             // To prevent aura staying on target after talent unlearned
+            case 18754: // Improved succubus - problems with apply if target is pet 
+                spellInfo->EffectApplyAuraName[0] = SPELL_AURA_ADD_FLAT_MODIFIER;    // it's affects duration of seduction, let's minimize affection 
+                spellInfo->EffectBasePoints[0] = -1.5*IN_MILLISECONDS*0.22;           // reduce cast time of seduction by 22%  
+                spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER; 
+                count++; 
+                break; 
+            case 18755: 
+                spellInfo->EffectApplyAuraName[0] = SPELL_AURA_ADD_FLAT_MODIFIER; 
+                spellInfo->EffectBasePoints[0] = -1.5*IN_MILLISECONDS*0.44;           //  reduce cast time of seduction by 44% 
+                spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER; 
+                count++; 
+                break; 
+            case 18756: 
+                spellInfo->EffectApplyAuraName[0] = SPELL_AURA_ADD_FLAT_MODIFIER; 
+                spellInfo->EffectBasePoints[0] = -1.5*IN_MILLISECONDS*0.66;           //  reduce cast time of seduction by 66% 
+                spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_CASTER; 
+                count++; 
+                break;
             case 48420:
                 spellInfo->Stances = 1 << (FORM_CAT - 1);
                 break;
