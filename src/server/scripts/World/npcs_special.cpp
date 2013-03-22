c408f3327ecba5d1db5e8ceaf854e00b0acf9c57
SaiNth100 <tonet24@hotmail.com>
2013-03-22 22:23:31 +0100

    Eliminada Constante del archivo que daba aviso.

--- src/server/scripts/World/npcs_special.cpp
+++ src/server/scripts/World/npcs_special.cpp
@@ -1989,7 +1989,7 @@ public:
               }
         }
 
-        void UpdateAI(const uint32 diff)
+        void UpdateAI(uint32 diff)
         {
            if (!me->isCharmed())
               me->DespawnOrUnsummon();
