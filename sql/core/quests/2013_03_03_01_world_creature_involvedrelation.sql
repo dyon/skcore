a45cba46e8cd2817f2606689c72319bb5659e78b
Oscar Ojeda <alfa_omega_96@hotmail.com>
2013-03-22 23:27:00 -0500

    Quest Arreglada: El destino de Ramaladni

---
+++ sql/core/quests/2013_03_03_01_world_creature_involvedrelation.sql
@@ -0,0 +1,7 @@
+-- Fix Quest The Fate of Ramaladni (El destino de Ramaladni)
+
+-- Se inserta para que finalice en Comandante de Cruzada Korfax 
+-- ya que no tiene a quien entregar a pesar que el addon lo indica
+
+INSERT INTO creature_involvedrelation
+VALUES (16112,9229);
